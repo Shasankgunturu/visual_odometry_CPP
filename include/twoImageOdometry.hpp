d0cb02531567b415a54fdfd7461cf976bc41754f
Shasank Gunturu <shasankgunturu@gmail.com>
2024-10-20 02:32:13 +0530

    threshold setup

--- include/twoImageOdometry.hpp
+++ include/twoImageOdometry.hpp
@@ -47,57 +47,65 @@ class Odometry {
     }
     */
     public:
-        cv::Mat image1, image2, K, C1, P;
-        double roll, pitch, yaw;
+    
+        cv::Mat     image1, image2, K, C1, P;
+        double      roll, pitch, yaw;
+
         cv::Mat getOdom(int threshold = 10, bool view=false) {
             bestKeypointFinderORB(threshold, view);
             return getPose();
         }
     private:
+
         std::vector<cv::Point2f> img1_pts, img2_pts;
+        
         void bestKeypointFinderORB(int threshold, bool view) {
-            cv::Ptr<cv::ORB> orb = cv::ORB::create();
-            std::vector<cv::KeyPoint> keypoints1, keypoints2;
-            cv::Mat descriptors1, descriptors2;
-            orb->detectAndCompute(image1, cv::Mat(), keypoints1, descriptors1);
-            orb->detectAndCompute(image2, cv::Mat(), keypoints2, descriptors2);
+            cv::Ptr<cv::ORB> orb            = cv::ORB::create();
+            std::vector<cv::KeyPoint>       keypoints1, keypoints2;
+            cv::Mat                         descriptors1, descriptors2;
+            std::vector<cv::DMatch>         matches;
+            std::vector<cv::DMatch>         best_matches;
+            
+            orb->detectAndCompute           (image1, cv::Mat(), keypoints1, descriptors1);
+            orb->detectAndCompute           (image2, cv::Mat(), keypoints2, descriptors2);
             cv::BFMatcher matcher(cv::NORM_HAMMING, true);
-            std::vector<cv::DMatch> matches;
             matcher.match(descriptors1, descriptors2, matches);
+
             int min_dist = 10000, max_dist = 0;
             for (int i=0; i<matches.size(); i++) {
-                if (matches[i].distance < min_dist) min_dist = matches[i].distance;
-                if (matches[i].distance > max_dist) max_dist = matches[i].distance;
+                if (matches[i].distance     < min_dist) min_dist = matches[i].distance;
+                if (matches[i].distance     > max_dist) max_dist = matches[i].distance;
             }
-            std::vector<cv::DMatch> best_matches;
             for (int i=0; i<matches.size(); i++) {
-                if (matches[i].distance <= 10*min_dist) {
-                    best_matches.push_back(matches[i]);
+                if (matches[i].distance     <= 10*min_dist) {
+                    best_matches.push_back  (matches[i]);
                 }
             }
             for (size_t i=0; i<best_matches.size(); i++) {
-                cv::Point2f point1 = keypoints1[best_matches[i].queryIdx].pt;
-                cv::Point2f point2 = keypoints2[best_matches[i].trainIdx].pt;
-                img1_pts.push_back(point1);
-                img2_pts.push_back(point2);
+                cv::Point2f point1          = keypoints1[best_matches[i].queryIdx].pt;
+                cv::Point2f point2          = keypoints2[best_matches[i].trainIdx].pt;
+                img1_pts.push_back          (point1);
+                img2_pts.push_back          (point2);
             }
             if (view) {
-                cv::Mat image1_with_points = image1.clone();
-                cv::Mat image2_with_points = image2.clone();
+                cv::Mat image1_with_points  = image1.clone();
+                cv::Mat image2_with_points  = image2.clone();
+                
                 for (size_t i = 0; i < img1_pts.size(); i++) {
-                    cv::circle(image1_with_points, img1_pts[i], 5, cv::Scalar(255, 0, 0), 2);
-                    cv::circle(image2_with_points, img2_pts[i], 5, cv::Scalar(255, 0, 0), 2);
+                    cv::circle              (image1_with_points, img1_pts[i], 5, cv::Scalar(255, 0, 0), 2);
+                    cv::circle              (image2_with_points, img2_pts[i], 5, cv::Scalar(255, 0, 0), 2);
                 }
-                std::cout << "No of matches: " << matches.size() << std::endl;
+
+                std::cout << "No of matches: "      << matches.size() << std::endl;
                 std::cout << "No of best matches: " << best_matches.size() << std::endl;
                 cv::imshow("Image 1 - Keypoints", image1_with_points);
                 cv::imshow("Image 2 - Keypoints", image2_with_points);
                 cv::waitKey(0);
             }
         }
+        
         cv::Mat convertFromHomogeneous(cv::Mat points4D) {
             cv::Mat points3D(points4D.rows, points4D.cols-1, points4D.type());
-
             for (int i = 0; i < points4D.rows; ++i) {
                 cv::Vec4f row = points4D.row(i);
                 double w = row[3];
@@ -107,75 +115,77 @@ class Odometry {
             }
             return points3D;
         }
+
         std::pair<cv::Mat, cv::Mat> getRT(cv::Mat essentialMatrix) {
-            cv::Mat R1, R2, _t;
-            cv::decomposeEssentialMat(essentialMatrix, R1, R2, _t);
-            std::vector<cv::Mat> R{R1,R1,R2,R2};
-            std::vector<cv::Mat> t{_t, -_t, _t, -_t};
-            std::vector<double> sum_z{0,0,0,0}, scale{0,0,0,0};
+            cv::Mat                     R1, R2, _t;
+            std::vector<cv::Mat>        R{R1,R1,R2,R2};
+            std::vector<cv::Mat>        t{_t, -_t, _t, -_t};
+            std::vector<double>         sum_z{0,0,0,0}, scale{0,0,0,0};
+            cv::decomposeEssentialMat   (essentialMatrix, R1, R2, _t);
+
             for (int i=0; i<R.size(); i++) {
-                cv::Mat T = cv::Mat::eye(4, 4, K.type());  
-                R[i].copyTo(T(cv::Rect(0, 0, 3, 3)));
-                t[i].copyTo(T(cv::Rect(3, 0, 1, 3)));              
-                cv::Mat K_ext;
-                cv::hconcat(K, cv::Mat::zeros(3, 1, K.type()), K_ext);
-                cv::Mat P_ext = K_ext * T;
-                cv::Mat point4D_1, point3D_1, point3D_2;
-                cv::triangulatePoints (P, P_ext, img1_pts, img2_pts, point4D_1);
-                cv::Mat T_changedType;
-                T.convertTo(T_changedType, point4D_1.type());
-                cv::Mat point4D_2 = T_changedType*point4D_1;
-                point3D_1 = convertFromHomogeneous(point4D_1.t());
-                point3D_2 = convertFromHomogeneous(point4D_2.t());
+                cv::Mat T               = cv::Mat::eye(4, 4, K.type());  
+                R[i].copyTo             (T(cv::Rect(0, 0, 3, 3)));
+                t[i].copyTo             (T(cv::Rect(3, 0, 1, 3)));              
+                cv::Mat                 K_ext, point4D_1, point3D_1, point3D_2, T_changedType;
+                cv::hconcat             (K, cv::Mat::zeros(3, 1, K.type()), K_ext);
+                cv::Mat P_ext           = K_ext * T;
+                cv::triangulatePoints   (P, P_ext, img1_pts, img2_pts, point4D_1);
+                T.convertTo             (T_changedType, point4D_1.type());
+                cv::Mat point4D_2       = T_changedType*point4D_1;
+                point3D_1               = convertFromHomogeneous(point4D_1.t());
+                point3D_2               = convertFromHomogeneous(point4D_2.t());
                 for (int j=0; j<point3D_1.rows; j++) {
-                    cv::Vec3f point1 = point3D_1.at<cv::Vec3f>(j);
-                    cv::Vec3f point2 = point3D_2.at<cv::Vec3f>(j);
+                    cv::Vec3f point1    = point3D_1.at<cv::Vec3f>(j);
+                    cv::Vec3f point2    = point3D_2.at<cv::Vec3f>(j);
                     if (point1[2]>0) {
-                        sum_z[i] = sum_z[i] + point1[2];    
+                        sum_z[i]        = sum_z[i] + point1[2];    
                     }
                     if (point2[2]>0) {
-                        sum_z[i] = sum_z[i] + point2[2];
+                        sum_z[i]        = sum_z[i] + point2[2];
                     }
                 }
                 for (int j=0; j<point3D_1.rows-1; j++) {
-                    cv::Vec3f p1_1 = point3D_1.at<cv::Vec3f>(j);
-                    cv::Vec3f p2_1 = point3D_1.at<cv::Vec3f>(j+1);
-                    cv::Vec3f p1_2 = point3D_2.at<cv::Vec3f>(j);
-                    cv::Vec3f p2_2 = point3D_2.at<cv::Vec3f>(j+1);
-                    scale[i] = scale[i] + (cv::norm(p1_1 - p1_2)/cv::norm(p2_1 - p2_2));
+                    cv::Vec3f p1_1      = point3D_1.at<cv::Vec3f>(j);
+                    cv::Vec3f p2_1      = point3D_1.at<cv::Vec3f>(j+1);
+                    cv::Vec3f p1_2      = point3D_2.at<cv::Vec3f>(j);
+                    cv::Vec3f p2_2      = point3D_2.at<cv::Vec3f>(j+1);
+                    scale[i]            = scale[i] + (cv::norm(p1_1 - p1_2)/cv::norm(p2_1 - p2_2));
                 }
-                scale[i] = scale[i]/(point3D_1.rows-1);
+                scale[i]                = scale[i]/(point3D_1.rows-1);
             }
-            double max_z = 0;
-            int return_index;
+            double max_z                = 0;
+            int                         return_index;
             for (int i=0; i<sum_z.size(); i++) {
                 if (sum_z[i]>max_z) {
-                    max_z = sum_z[i];
-                    return_index = i;
+                    max_z               = sum_z[i];
+                    return_index        = i;
                 }
             }
-            t[return_index] = t[return_index]*scale[return_index];
+            t[return_index]             = t[return_index]*scale[return_index];
+
             return std::make_pair(R[return_index], t[return_index]);
         }
+
         cv::Mat getPose() {
-            cv::Mat essentialMatrix;
-            cv::Mat mask;
-            essentialMatrix = cv::findEssentialMat(img1_pts, img2_pts, K, cv::RANSAC,  0.999, 1.0, mask);
-            auto RT = getRT(essentialMatrix);
-            cv::Mat R = RT.first;
-            cv::Mat t = RT.second;
-            rotationMatrixToEulerAngles(R, t);
-            C1 = R * t;
-            cv::Mat transform = cv::Mat::eye(4, 4, K.type());  
-            R.copyTo(transform(cv::Rect(0, 0, 3, 3)));
-            t.copyTo(transform(cv::Rect(3, 0, 1, 3)));   
+            cv::Mat                     essentialMatrix, mask;
+            essentialMatrix             = cv::findEssentialMat(img1_pts, img2_pts, K, cv::RANSAC,  0.999, 1.0, mask);
+            auto RT                     = getRT(essentialMatrix);
+            cv::Mat R                   = RT.first;
+            cv::Mat t                   = RT.second;
+            rotationMatrixToEulerAngles (R, t);
+            C1                          = R * t;
+            cv::Mat transform           = cv::Mat::eye(4, 4, K.type());  
+            R.copyTo                    (transform(cv::Rect(0, 0, 3, 3)));
+            t.copyTo                    (transform(cv::Rect(3, 0, 1, 3)));   
             return transform;
         }
+        
         void rotationMatrixToEulerAngles(cv::Mat R, cv::Mat t) {
-            assert(R.rows == 3 && R.cols == 3);
-            pitch = atan2(-R.at<double>(2, 0), sqrt(R.at<double>(2, 1) * R.at<double>(2, 1) + R.at<double>(2, 2) * R.at<double>(2, 2)));
-            yaw = atan2(R.at<double>(1, 0), R.at<double>(0, 0));
-            roll = atan2(R.at<double>(2, 1), R.at<double>(2, 2));
+            assert  (R.rows == 3 && R.cols == 3);
+            pitch   = atan2(-R.at<double>(2, 0), sqrt(R.at<double>(2, 1) * R.at<double>(2, 1) + R.at<double>(2, 2) * R.at<double>(2, 2)));
+            yaw     = atan2(R.at<double>(1, 0), R.at<double>(0, 0));
+            roll    = atan2(R.at<double>(2, 1), R.at<double>(2, 2));
         }
 };
 
--- src/odometry_.cpp
+++ src/odometry_.cpp
@@ -1,7 +1,23 @@
 #include<twoImageOdometry.hpp>
 #include <fstream>
-#define NUM_FRAMES 55
-#define FIRST_FRAME 12
+
+// Modify the below parameters before executing the code.
+#define NUM_FRAMES 301
+#define FIRST_FRAME 15
+#define SKIPPING_THRESHOLD 10
+#define MATCHES_THRESHOLD 10
+
+std::string folder                  = "/home/shasankgunturu/personal/ComputerVisionBasics/src/images/data1";
+std::string image_format            = ".png";
+std::string estimated_poses_path    = "/home/shasankgunturu/personal/ComputerVisionBasics/src/output/poses.txt";
+cv::Mat K                           = (cv::Mat_<double>(3, 3) << 
+                                        1253.1012617250694, 0.0, 471.6666914725001, 
+                                        0.0, 1233.192145117128, 327.9885858399886, 
+                                        0.0, 0.0, 1.0);
+cv::Mat P                           = (cv::Mat_<double>(3,4) <<
+                                        1165.624268, 0.000000, 418.089806, 0.000000,
+                                        0.000000, 1252.924683, 333.552941, 0.000000,
+                                        0.000000, 0.000000, 1.000000, 0.000000);
 
 void savePosesToFile(const std::vector<std::vector<double>>& poses, const std::string& filename) {
     std::ofstream outfile(filename);
@@ -17,46 +33,40 @@ void savePosesToFile(const std::vector<std::vector<double>>& poses, const std::s
 
 
 int main() {
-    cv::Mat K       = (cv::Mat_<double>(3, 3) << 
-                            1253.1012617250694, 0.0, 471.6666914725001, 
-                            0.0, 1233.192145117128, 327.9885858399886, 
-                            0.0, 0.0, 1.0);
-    cv::Mat P       = (cv::Mat_<double>(3,4) <<
-                            1165.624268, 0.000000, 418.089806, 0.000000,
-                            0.000000, 1252.924683, 333.552941, 0.000000,
-                            0.000000, 0.000000, 1.000000, 0.000000);
-    cv::Mat image1  = cv::imread("/home/shasankgunturu/personal/ComputerVisionBasics/src/images/data/captured_image_"+ std::to_string(FIRST_FRAME) +".jpg", cv::IMREAD_GRAYSCALE);
-    cv::Mat image2; 
-    // if (image1.empty() || image2.empty()) {
-    //     std::cerr << "can't find images" << std::endl;
-    //     return -1;
-    // }
+    cv::Mat image2, curr_pose, image1;     
+    image1              = cv::imread(folder+"/captured_image_"+ std::to_string(FIRST_FRAME) +image_format, cv::IMREAD_GRAYSCALE);
     
-    Odometry        o;
-    o.K             = K;
-    o.P             = P;
-    cv::Mat curr_pose;
+    Odometry            o;
+    o.K                 = K;
+    o.P                 = P;
+    
     std::vector<std::vector<double>> final_poses;
+
+    // Loop to process the images
     for (int i=FIRST_FRAME+1; i<NUM_FRAMES; i++) {
-        image2  = cv::imread("/home/shasankgunturu/personal/ComputerVisionBasics/src/images/data/captured_image_"+ std::to_string(i) +".jpg", cv::IMREAD_GRAYSCALE);
-        o.image1        = image1;
-        o.image2        = image2;
-        cv::Mat new_transform = o.getOdom(10);
-        cv::Mat new_pose;
-        // std::cout << new_transform << std::endl;
+        cv::Mat                     new_pose;
+        image2                      = cv::imread(folder+"/captured_image_"+ std::to_string(i) +image_format, cv::IMREAD_GRAYSCALE);
+        o.image1                    = image1;
+        o.image2                    = image2;
+        cv::Mat new_transform       = o.getOdom(MATCHES_THRESHOLD);
         if (i==FIRST_FRAME+1) {
-            new_pose = new_transform;
+            new_pose                = new_transform;
         }
         else {
-            new_pose = curr_pose*new_transform.inv();
+            new_pose                = curr_pose*new_transform.inv();
+            cv::Mat tester          = new_pose-curr_pose;
+            cv::Vec4f test_point    = tester.col(3);
+            // Check for Outliers
+            if ((test_point[0]>SKIPPING_THRESHOLD) || (test_point[1]>SKIPPING_THRESHOLD) || (test_point[2]>SKIPPING_THRESHOLD)) {
+                std::cout << "Skipping Frame: " << i-FIRST_FRAME << std::endl;
+                continue;
+            }
         }
-        // std::cout << new_pose;
-        curr_pose = new_pose;
-        cv::Vec4f estimated_point = new_pose.col(3);
+        curr_pose                   = new_pose;
+        image1                      = image2;
+        cv::Vec4f estimated_point   = new_pose.col(3);
+        std::cout                   << "Handling Image: " << i-FIRST_FRAME << std::endl;
         final_poses.push_back({estimated_point[0], estimated_point[1], estimated_point[2]});
-        image1 = image2;
-        std::cout << "Handling Image: " << i-FIRST_FRAME << std::endl;
     }
-    std::string filename = "/home/shasankgunturu/personal/ComputerVisionBasics/src/output/poses.txt";
-    savePosesToFile(final_poses, filename);
+    savePosesToFile(final_poses, estimated_poses_path);
 }
